016a8eb4528ac5f65ca6b20c5b6280bf7fa54767
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-04-11 23:08:06 -0400

    refactored routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { Children } from 'react'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom-v5-compat'
+import './index.css'
 import { PostsList } from './features/posts/postsList'
 
 import { Navbar } from './app/Navbar'
+const router = createBrowserRouter([
+  {
+    element: <Navbar />,
+    children: [
+      {
+        path: '/',
+        element: (
+          <React.Fragment>
+            <PostsList />
+          </React.Fragment>
+        ),
+      },
+    ],
+  },
+])
 
 function App() {
   return (
-    <Router>
+    /*<Router>
       <Navbar />
       <div className="App">
         <Switch>
@@ -27,7 +44,8 @@ function App() {
           <Redirect to="/" />
         </Switch>
       </div>
-    </Router>
+    </Router>*/
+    <RouterProvider router={router} />
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+
 import App from './App'
 import store from './app/store'
 import { Provider } from 'react-redux'
 
 import { worker } from './api/server'
-
 // Wrap app rendering so we can wait for the mock API to initialize
 async function start() {
   // Start our mock API server
