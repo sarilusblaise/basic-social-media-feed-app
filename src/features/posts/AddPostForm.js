8a3a9e4e8bf755ed21e3f5be14679c4888b5ea84
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-04-14 16:46:07 -0400

    added behavior to form button

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,11 +1,28 @@
 import React, { useState } from 'react'
-
+import { useDispatch } from 'react-redux'
+import { postAdded } from './postsSlice'
+import { nanoid } from '@reduxjs/toolkit'
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
 
   const onTitleChanged = (e) => setTitle(e.target.value)
   const onContentChanged = (e) => setContent(e.target.value)
+  const dispatch = useDispatch()
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(
+        postAdded({
+          id: nanoid(),
+          title,
+          content,
+        })
+      )
+
+      setTitle('')
+      setContent('')
+    }
+  }
 
   return (
     <section>
@@ -26,7 +43,9 @@ export const AddPostForm = () => {
           value={content}
           onChange={onContentChanged}
         />
-        <button type="button">Save Post</button>
+        <button type="button" onClick={onSavePostClicked}>
+          Save Post
+        </button>
       </form>
     </section>
   )
