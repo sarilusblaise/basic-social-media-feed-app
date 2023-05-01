df9bded0d53883695b15ead884751c1ac1b40c77
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-04-30 22:32:33 -0400

    dispatched action in emoji buttons

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -48,8 +48,15 @@ const postsSlice = createSlice({
         existingPost.content = content
       }
     },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find((post) => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
+    },
   },
 })
 
-export const { postAdded, postUpdated } = postsSlice.actions
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 export default postsSlice.reducer
