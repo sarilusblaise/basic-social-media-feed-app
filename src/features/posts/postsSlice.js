931d7f289c003f46500e229edca497affbb38ff2
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-05-29 22:57:19 -0400

    handle fetch post in extraReducers

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -45,6 +45,21 @@ const postsSlice = createSlice({
       }
     },
   },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        // Add any fetched posts to the array
+        state.posts = state.posts.concat(action.payload)
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
+  },
 })
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
   const response = await client.get('/fakeApi/posts')
