ef6ba1249c6b37e8664b78347aec62168e2142d9
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-06-29 21:50:49 -0400

    fetched users

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import store from './app/store'
 import { Provider } from 'react-redux'
+import { fetchUsers } from './features/users/usersSlice'
 
 import { worker } from './api/server'
 // Wrap app rendering so we can wait for the mock API to initialize
 async function start() {
   // Start our mock API server
   await worker.start({ onUnhandledRequest: 'bypass' })
+  store.dispatch(fetchUsers())
 
   ReactDOM.render(
     <React.StrictMode>
