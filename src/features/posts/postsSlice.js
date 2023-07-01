e4de548597f472bcf7d0babd58900770013124a6
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-07-01 12:46:24 -0400

    created addNewPost async action creator

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -8,6 +8,17 @@ const initialState = {
   error: null,
 }
 
+export const addNewPost = createAsyncThunk(
+  'posts/addNewPost',
+  // The payload creator receives the partial `{title, content, user}` object
+  async (initialPost) => {
+    // We send the initial data to the fake API server
+    const response = await client.post('/fakeApi/posts', initialPost)
+    // The response includes the complete post object, including unique ID
+    return response.data
+  }
+)
+
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
@@ -59,6 +70,10 @@ const postsSlice = createSlice({
         state.status = 'failed'
         state.error = action.error.message
       })
+    builder.addCase(addNewPost.fulfilled, (state, action) => {
+      // We can directly add the new post object to our posts array
+      state.posts.push(action.payload)
+    })
   },
 })
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
